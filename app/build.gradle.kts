48b2c07709b8f8053e4b3e7fd2120f5c046bba85
Alheta Wahyu M. <alheta.matalarens@gmail.com>
2024-10-25 16:36:18 +0700

    moved libraries to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.parcelize)
-    kotlin("plugin.serialization") version "2.0.20"
-    id("com.google.devtools.ksp")
-    id("dagger.hilt.android.plugin")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -60,47 +60,31 @@ android {
 }
 
 dependencies {
-
+    // Core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.runtime.ktx)
-
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.navigation.compose)
-
-    //TODO: Move dependencies to Version Catalog
-    //Lottie
-    val lottieVersion = "6.0.0"
-    implementation("com.airbnb.android:lottie-compose:$lottieVersion")
-
-    //Hilt
-    implementation ("com.google.dagger:hilt-android:2.50")
-    ksp ("com.google.dagger:hilt-android-compiler:2.47")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
-    //Kotlin Serializable
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    // Retrofit for API requests
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    // OkHttp for logging
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    // ViewModel and LiveData for MVVM architecture
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    //Coil for image loading
-    implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc01")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc01")
-    // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-
     implementation(libs.androidx.appcompat)
-
+    // Lifecycle Bundles
+    implementation (libs.bundles.lifecycle)
+    // Compose Bundle
+    implementation(libs.bundles.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    // Navigation Bundles
+    implementation(libs.bundles.navigation)
+    // Network Bundles
+    implementation(libs.bundles.network)
+    //Kotlin Serializable
+    implementation(libs.kotlinx.serialization.json)
+    //Hilt
+    implementation (libs.hilt.android)
+    ksp (libs.hilt.android.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+    //Coil Bundles
+    implementation(libs.bundles.coil)
+    //Lottie
+    implementation(libs.lottie.compose)
+    // Testing Libs
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
